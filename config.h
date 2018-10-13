bac2905b27001cc326ea2c2fc189b579d90f9445
Phong Nguyen <nhphong1406@gmail.com>
2018-10-14 04:25:53 +0700

    Updates mappings and Adds keymap info

--- config.h
+++ config.h
@@ -27,3 +27,8 @@
 
 #define TAPPING_TERM    200
 #define IGNORE_MOD_TAP_INTERRUPT // this makes it possible to do rolling combos (zx) with keys that convert to other keys on hold (z becomes ctrl when you hold it, and when this option isn't enabled, z rapidly followed by x actually sends Ctrl-x. That's bad.)
+
+#define GRAVE_ESC_ALT_OVERRIDE   1
+#define GRAVE_ESC_CTRL_OVERRIDE  1
+#define GRAVE_ESC_GUI_OVERRIDE   1
+#define GRAVE_ESC_SHIFT_OVERRIDE 1
