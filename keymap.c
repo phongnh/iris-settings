f38e627a49a8bc55fc938a0bf37557e758709ad3
Phong Nguyen <nhphong1406@gmail.com>
2018-10-14 20:26:34 +0700

    Swap Space and Enter keys

--- keymap.c
+++ keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------+------+------.          ,------+------+------+------+------+------+------|
      * | Shift|  Z   |  X   |  C   |   V  |  B   |  `   |          | Esc  |  N   |  M   |  ,   |  .   |  /   |Enter |
      * `------+------+------+------+------+------+------|          |------+------+------+------+------+------+------'
-     *                             | LGUI |LOWER |Space |          |Enter |RAISE | LALT  |
+     *                             | LGUI |LOWER |Enter |          |Space |RAISE | LALT  |
      *                             `------+------+------'          `-----'-------+------'
      */
     [_QWERTY] = LAYOUT(
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,         KC_Q,           KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,                 KC_BSPC,
         CTL_T(KC_ESC) , KC_A,           KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    LT(_MOUSE, KC_SCLN),  RGUI_T(KC_QUOT),
         KC_LSFT,        CTL_T(KC_Z),    KC_X,    KC_C,    KC_V,    KC_B,    KC_ESC,           KC_GRV,  KC_N,    KC_M,    KC_COMM, KC_DOT,  CTL_T(KC_SLSH),       RSFT_T(KC_ENT),
-                                                          KC_LGUI, LOWER,   KC_SPC,           KC_ENT,  RAISE,   KC_LALT
+                                                          KC_LGUI, LOWER,   KC_ENT,           KC_SPC,  RAISE,   KC_LALT
     ),
 
     /*
