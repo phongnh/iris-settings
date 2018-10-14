5f9f73efb4edb29fe2bdafd6b62f76b9c1059c73
Phong Nguyen <nhphong1406@gmail.com>
2018-10-15 05:05:59 +0700

    Use correct keycodes for [ and ]

--- keymap.c
+++ keymap.c
@@ -99,7 +99,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_F12,         KC_F1,          KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,               KC_F11,
         KC_TILD,        KC_EXLM,        KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,              KC_F12,
         _______,        _______,        _______, _______, KC_PGUP, KC_UNDS,                            KC_EQL,  KC_HOME, KC_GRV,  KC_ESC,  KC_COLN,              KC_BSLS,
-        _______,        _______,        _______, _______, KC_PGDN, KC_MINS, KC_LCBR,          KC_RCBR, KC_PLUS, KC_END,  _______, _______, _______,              _______,
+        _______,        _______,        _______, _______, KC_PGDN, KC_MINS, KC_LBRC,          KC_RBRC, KC_PLUS, KC_END,  _______, _______, _______,              _______,
                                                           _______, _______, KC_BSPC,          _______, _______, _______
     ),
 
