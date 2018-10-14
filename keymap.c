752e793431d57986b3b0de12279d3fe5cb88f073
Phong Nguyen <nhphong1406@gmail.com>
2018-10-15 04:37:27 +0700

    Adds LAGT_T mapping and Update () to [] on RAISE layer

--- keymap.c
+++ keymap.c
@@ -21,13 +21,13 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
      * ,-----------------------------------------.                        ,-----------------------------------------.
-     * |  =   |  1   |  2   |  3   |   4  |  5   |                        |  6   |  7   |  8   |  9   |  0   |  -   |
+     * |  =   |  1   |  2   |  3   |  4   |  5   |                        |  6   |  7   |  8   |  9   |  0   |  -   |
      * |------+------+------+------+------+------|                        |------+------+------+------+------+------|
-     * | Tab  |  Q   |  W   |  E   |   R  |  T   |                        |  Y   |  U   |  I   |  O   |  P   | Bksp |
+     * | Tab  |  Q   |  W   |  E   |  R   |  T   |                        |  Y   |  U   |  I   |  O   |  P   | Bksp |
      * |------+------+------+------+------+------|                        |------+------+------+------+------+------|
-     * | Ctrl |  A   |  S   |  D   |   F  |  G   |                        |  H   |  J   |  K   |  L   |  ;   |  "   |
+     * | Ctrl |  A   |  S   |  D   |  F   |  G   |                        |  H   |  J   |  K   |  L   |  ;   |  "   |
      * |------+------+------+------+------+------+------.          ,------+------+------+------+------+------+------|
-     * | Shift|  Z   |  X   |  C   |   V  |  B   |  `   |          | Esc  |  N   |  M   |  ,   |  .   |  /   |Enter |
+     * | Shift|  Z   |  X   |  C   |  V   |  B   |  `   |          | Esc  |  N   |  M   |  ,   |  .   |  /   |Enter |
      * `------+------+------+------+------+------+------|          |------+------+------+------+------+------+------'
      *                             | LGUI |LOWER |Enter |          |Space |RAISE | LALT  |
      *                             `------+------+------'          `-----'-------+------'
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_EQL,         KC_1,           KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,                 KC_MINS,
         KC_TAB,         KC_Q,           KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,                 KC_BSPC,
         CTL_T(KC_ESC) , KC_A,           KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    LT(_MOUSE, KC_SCLN),  RGUI_T(KC_QUOT),
-        KC_LSFT,        CTL_T(KC_Z),    KC_X,    KC_C,    KC_V,    KC_B,    KC_ESC,           KC_GRV,  KC_N,    KC_M,    KC_COMM, KC_DOT,  CTL_T(KC_SLSH),       RSFT_T(KC_ENT),
+        KC_LSFT,        CTL_T(KC_Z),    KC_X,    KC_C,    KC_V,    KC_B,    LCAG_T(KC_GRV),   KC_GRV,  KC_N,    KC_M,    KC_COMM, KC_DOT,  CTL_T(KC_SLSH),       RSFT_T(KC_ENT),
                                                           KC_LGUI, LOWER,   KC_ENT,           KC_SPC,  RAISE,   KC_LALT
     ),
 
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ,-----------------------------------------.                        ,-----------------------------------------.
      * |  ~   |  !   |  @   |  #   |  $   |  %   |                        |  ^   |  &   |  *   |  (   |  )   | Bksp |
      * |------+------+------+------+------+------|                        |------+------+------+------+------+------|
-     * |Capslk|  1   |  2   |  3   |   4  |  5   |                        |  6   |  7   |  8   |  9   |  0   | Del  |
+     * |Capslk|  1   |  2   |  3   |  4   |  5   |                        |  6   |  7   |  8   |  9   |  0   | Del  |
      * |------+------+------+------+------+------|                        |------+------+------+------+------+------|
      * |      |  `   | Left | Right|  Up  |  [   |                        |  ]   |  4   |  5   |  6   |  +   |  |   |
      * |------+------+------+------+------+------+------.          ,------+------+------+------+------+------+------|
@@ -90,7 +90,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------+------|                        |------+------+------+------+------+------|
      * |      |      |      |      | PgUp |  _   |                        |  =   | Home | Esc  |  `   |  :   |  /   |
      * |------+------+------+------+------+------+------.          ,------+------+------+------+------+------+------|
-     * |      |      |      |      | PgDn |  -   |  (   |          |   )  |  +   | End  |      |      |      |      |
+     * |      |      |      |      | PgDn |  -   |  [   |          |   ]  |  +   | End  |      |      |      |      |
      * `------+------+------+------+------+------+------|          |------+------+------+------+------+------+------'
      *                             |      |      | Bksp |          |      |      |      |
      *                             `------+------+------'          `-----'-------+------'
@@ -99,7 +99,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_F12,         KC_F1,          KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,               KC_F11,
         KC_TILD,        KC_EXLM,        KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,              KC_F12,
         _______,        _______,        _______, _______, KC_PGUP, KC_UNDS,                            KC_EQL,  KC_HOME, KC_GRV,  KC_ESC,  KC_COLN,              KC_BSLS,
-        _______,        _______,        _______, _______, KC_PGDN, KC_MINS, KC_LPRN,          KC_RPRN, KC_PLUS, KC_END,  _______, _______, _______,              _______,
+        _______,        _______,        _______, _______, KC_PGDN, KC_MINS, KC_LCBR,          KC_RCBR, KC_PLUS, KC_END,  _______, _______, _______,              _______,
                                                           _______, _______, KC_BSPC,          _______, _______, _______
     ),
 
