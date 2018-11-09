f7d0c03f2e1818c22fc14c6f7e406e4e7f7f6479
Phong Nguyen <nhphong1406@gmail.com>
2018-11-09 10:04:07 +0700

    Combines Iris Snow with current mappings

--- keymap.c
+++ keymap.c
@@ -11,13 +11,12 @@ enum iris_layers {
     _MOUSE,
 };
 
-enum custom_keycodes {
-    QWERTY = SAFE_RANGE,
+enum iris_keycodes {
+    EPRM = SAFE_RANGE,
+    VRSN,
+    QWERTY,
     LOWER,
     RAISE,
-    ADJUST,
-    EPRM,
-    VRSN,
     MOUSE,
 };
 
@@ -36,11 +35,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *                             `------+------+------'          `------'------+------'
      */
     [_QWERTY] = LAYOUT(
-        KC_EQL,         KC_1,           KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,                 KC_MINS,
-        KC_TAB,         KC_Q,           KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,                 KC_BSPC,
-        CTL_T(KC_ESC) , KC_A,           KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    LT(_MOUSE, KC_SCLN),  RGUI_T(KC_QUOT),
-        KC_LSFT,        CTL_T(KC_Z),    KC_X,    KC_C,    KC_V,    KC_B,    LCAG_T(KC_GRV),   KC_ESC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  CTL_T(KC_SLSH),       KC_RSFT,
-                                                          KC_LGUI, LOWER,   KC_SPC,           KC_ENT,  RAISE,   KC_LALT
+        KC_EQL,        KC_1,           KC_2,    KC_3,    KC_4,    KC_5,                                 KC_6,   KC_7,                KC_8,      KC_9,         KC_0,                 KC_MINS,
+        KC_TAB,        KC_Q,           KC_W,    KC_E,    KC_R,    KC_T,                                 KC_Y,   KC_U,                KC_I,      KC_O,         KC_P,                 KC_BSPC,
+        CTL_T(KC_ESC), KC_A,           KC_S,    KC_D,    KC_F,    KC_G,                                 KC_H,   KC_J,                KC_K,      KC_L,         LT(_MOUSE, KC_SCLN),  RGUI_T(KC_QUOT),
+        KC_LSFT,       CTL_T(KC_Z),    KC_X,    KC_C,    KC_V,    KC_B,      LCAG_T(KC_GRV),   KC_ESC,  KC_N,   KC_M,                KC_COMM,   KC_DOT,       CTL_T(KC_SLSH),       KC_RSFT,
+                                                         KC_LGUI, LT(_LOWER, KC_LBRC),         KC_SPC,  KC_ENT, LT(_RAISE, KC_RBRC), KC_LALT
     ),
 
     /*
@@ -61,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,         KC_1,           KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,                 KC_DEL,
         KC_CAPS,        _______,        KC_LEFT, KC_RGHT, KC_UP,   KC_LBRC,                            KC_RBRC, KC_P4,   KC_P5,   KC_P6,   KC_PLUS,              KC_PIPE,
         _______,        _______,        _______, _______, KC_DOWN, KC_LCBR, KC_LPRN,          KC_RPRN, KC_RCBR, KC_P1,   KC_P2,   KC_P3,   KC_MINS,              _______,
-                                                          _______, _______, _______,          KC_DEL,  _______, KC_P0
+                                                          _______, LOWER,   _______,          KC_DEL,  RAISE,   KC_P0
     ),
 
     /*
@@ -70,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------+------|                        |------+------+------+------+------+------|
      * |  ~   |  !   |  @   |  #   |  $   |  %   |                        |  ^   |  &   |  *   |  (   |  )   | F12  |
      * |------+------+------+------+------+------|                        |------+------+------+------+------+------|
-     * |      |      |      |      | PgUp |  _   |                        |  =   | Home | Esc  |  `   |  :   |  /   |
+     * |      |      |      |      | PgUp |  _   |                        |  =   | Home |      |      |  :   |  /   |
      * |------+------+------+------+------+------+------.          ,------+------+------+------+------+------+------|
      * |      |      |      |      | PgDn |  -   |  (   |          |   )  |  +   | End  |      |      |      |      |
      * `------+------+------+------+------+------+------|          |------+------+------+------+------+------+------'
@@ -80,9 +79,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_RAISE] = LAYOUT(
         KC_F12,         KC_F1,          KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,               KC_F11,
         KC_TILD,        KC_EXLM,        KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,              KC_F12,
-        _______,        _______,        _______, _______, KC_PGUP, KC_UNDS,                            KC_EQL,  KC_HOME, KC_ESC,  KC_GRV,  KC_COLN,              KC_BSLS,
+        _______,        _______,        _______, _______, KC_PGUP, KC_UNDS,                            KC_EQL,  KC_HOME, _______, _______, KC_COLN,              KC_BSLS,
         _______,        _______,        _______, _______, KC_PGDN, KC_MINS, KC_LPRN,          KC_RPRN, KC_PLUS, KC_END,  _______, _______, _______,              _______,
-                                                          _______, _______, KC_DEL,           _______, _______, _______
+                                                          _______, LOWER,   KC_DEL,           _______, RAISE,   _______
     ),
 
     /*
@@ -128,60 +127,52 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     )
 };
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+bool process_record_user(uint16_t keycode, keyrecord_t* record) {
     switch (keycode) {
-        case QWERTY:
-            if (record->event.pressed) {
-                set_single_persistent_default_layer(_QWERTY);
-            }
-            return false;
-            break;
-        case LOWER:
-            if (record->event.pressed) {
-                layer_on(_LOWER);
-                update_tri_layer(_LOWER, _RAISE, _ADJUST);
-            } else {
-                layer_off(_LOWER);
-                update_tri_layer(_LOWER, _RAISE, _ADJUST);
-            }
-            return false;
-            break;
-        case RAISE:
-            if (record->event.pressed) {
-                layer_on(_RAISE);
-                update_tri_layer(_LOWER, _RAISE, _ADJUST);
-            } else {
-                layer_off(_RAISE);
-                update_tri_layer(_LOWER, _RAISE, _ADJUST);
-            }
-            return false;
-            break;
-        case ADJUST:
-            if (record->event.pressed) {
-                layer_on(_ADJUST);
-            } else {
-                layer_off(_ADJUST);
-            }
-            return false;
-            break;
-        case MOUSE:
-            if (record->event.pressed) {
-                set_single_persistent_default_layer(_MOUSE);
-            }
-            return false;
-            break;
-        case EPRM:
-            if (record->event.pressed) {
-                eeconfig_init();
-            }
-            return false;
-            break;
-        case VRSN:
-            if (record->event.pressed) {
-                SEND_STRING (QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
-            }
-            return false;
-            break;
+    case EPRM:
+        if (record->event.pressed) {
+            eeconfig_init();
+        }
+        return false;
+        break;
+    case VRSN:
+        if (record->event.pressed) {
+            SEND_STRING(QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
+        }
+        return false;
+        break;
+    case QWERTY:
+        if (record->event.pressed) {
+            set_single_persistent_default_layer(_QWERTY);
+        }
+        return false;
+        break;
+    case LOWER:
+        if (record->event.pressed) {
+            layer_on(_LOWER);
+            update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        } else {
+            layer_off(_LOWER);
+            update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        }
+        return false;
+        break;
+    case RAISE:
+        if (record->event.pressed) {
+            layer_on(_RAISE);
+            update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        } else {
+            layer_off(_RAISE);
+            update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        }
+        return false;
+        break;
+    case MOUSE:
+        if (record->event.pressed) {
+            set_single_persistent_default_layer(_MOUSE);
+        }
+        return false;
+        break;
     }
     return true;
 }
