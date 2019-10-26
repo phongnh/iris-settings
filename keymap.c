1ae9e56e5b15c8902f3861227c224a397625c646
Phong Nguyen <nhphong1406@gmail.com>
2019-10-26 23:52:02 +0700

    Fix mapping

--- keymap.c
+++ keymap.c
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     [_PHONG] = LAYOUT(
         KC_EQL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                           KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
-        CAG_TAB, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+        CAG_TAB, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
         CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                           KC_H,    KC_J,    KC_K,    KC_L,    MOU_SCL, GUI_QUO,
         KC_LSFT, CTL_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC,      KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  CTL_SLS, SFT_BSP,
                                             KC_LGUI, LWR_SPC, KC_SPC,       KC_ENT,  RSE_ENT, KC_LALT
